c7b83981206904b8adb2ad2e6a13f76c53f4596e
bitcat <kurohaj96@gmail.com>
2025-03-16 02:45:28 +0900

    ネストを排除し、ショートサーキットを利用

---
+++ test.js
@@ -0,0 +1,26 @@
+const fizzBuzz = Array.from({ length: 100 }, (_, i) => i + 1)
+  .map(n => {
+    const fizz = n % 3 === 0;
+    const buzz = n % 5 === 0;
+    return (fizz && buzz && 'FizzBuzz') || (fizz && 'Fizz') || (buzz && 'Buzz') || n;
+  });
+
+
+const AddPatterns = numbers => 
+  numbers.map(n => {
+    const aho = n % 3 === 0 || n.toString().includes('3');
+    const quzz = n % 7 === 0;
+    const kass = n % 11 === 0;
+    
+    const result = [
+      aho ? 'Aho' : '',
+      quzz ? 'Quzz' : '',
+      kass ? 'Kass' : ''
+    ].join('');
+
+    return result || n;
+  });
+
+const addedFizzBuzz = AddPatterns(fizzBuzz);
+
+console.log(addedFizzBuzz);
